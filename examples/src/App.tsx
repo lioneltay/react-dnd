2412c50cdb4162c732173c5fea8de7df8a883ab2
Lionel Tay <lionel.lt.tay@gmail.com>
2019-01-07 18:32:06 +1100

    Setup testing with jest and npm scripts hooks

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,12 +1,11 @@
 import React, { Component } from "react"
-import { randomInt, DemoComponent } from "mymodule"
+import { DemoComponent } from "mymodule"
 
 class App extends Component {
   render() {
     return (
       <div>
         <DemoComponent />
-        <div>Random Number: {randomInt(5, 10)}</div>
       </div>
     )
   }
---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+  // A set of global variables that need to be available in all test environments
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json",
+    },
+  },
+  // An array of file extensions your modules use
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  // The glob patterns Jest uses to detect test files
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  // A map from regular expressions to paths to transformers
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+}
